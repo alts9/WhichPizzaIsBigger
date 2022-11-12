6c61c07e76c1aab62e0aed9f7c1dc2b6ce6bdf14
Alts <alts0009@gmail.com>
2022-11-12 08:34:47 +0700

    Add popular comparison

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const elements = (() => {
     const addButton = document.querySelector('.addButton');
     const checkboxCrust = document.getElementById('checkboxCrust');
     const checkboxPrice = document.getElementById('checkboxPrice');
+    const popular1 = document.getElementById('popular1');
+    const popular2 = document.getElementById('popular2');
+    const popular3 = document.getElementById('popular3');
+    const popular4 = document.getElementById('popular4');
+    const popular5 = document.getElementById('popular5');
     const table = document.querySelector('table')
     let allDelete = document.querySelectorAll('.deleteButton');
     let allShownPizza = document.querySelectorAll('.pizza');
@@ -12,7 +17,8 @@ const elements = (() => {
     let allPrice = document.querySelectorAll('.priceInput');
     return {
         pizzaList, addRow, allDelete, allShownPizza, allDiameter, allQuantity,
-        allPrice, addButton, checkboxCrust, checkboxPrice, table
+        allPrice, addButton, checkboxCrust, checkboxPrice, table,
+        popular1, popular2, popular3, popular4, popular5
     };
 })();
 
@@ -20,6 +26,11 @@ function generalEvent(){
     elements.addButton.onmousedown = () => addNewPizza();
     elements.checkboxCrust.oninput = () => toggleCrust(checkboxCrust);
     elements.checkboxPrice.oninput = () => togglePrice(checkboxPrice);
+    elements.popular1.onmousedown = () => popular1();
+    elements.popular2.onmousedown = () => popular2();
+    elements.popular3.onmousedown = () => popular3();
+    elements.popular4.onmousedown = () => popular4();
+    elements.popular5.onmousedown = () => popular5();
 }
 
 function togglePrice(checkboxPrice){
@@ -280,10 +291,50 @@ function refreshDisplayedPizza(){
 
 function popular1(){
     allPizza = [];
-    let pizza2 = new Pizza(17, 1, 7)
+    let pizza2 = new Pizza(12, 2, 0)
     allPizza.push(pizza2);
-    let pizza1 = new Pizza(15, 1, 7)
+    let pizza1 = new Pizza(18, 1, 0)
     allPizza.push(pizza1);
+    allrefresh();
+}
+
+function popular2(){
+    allPizza = [];
+    let pizza2 = new Pizza(16, 1, 0)
+    allPizza.push(pizza2);
+    let pizza1 = new Pizza(18, 1, 0)
+    allPizza.push(pizza1);
+    allrefresh();
+}
+
+function popular3(){
+    allPizza = [];
+    let pizza2 = new Pizza(12, 2, 0)
+    allPizza.push(pizza2);
+    let pizza1 = new Pizza(16, 1, 0)
+    allPizza.push(pizza1);
+    allrefresh();
+}
+
+function popular4(){
+    allPizza = [];
+    let pizza2 = new Pizza(8, 4, 0)
+    allPizza.push(pizza2);
+    let pizza1 = new Pizza(16, 1, 0)
+    allPizza.push(pizza1);
+    allrefresh();
+}
+
+function popular5(){
+    allPizza = [];
+    let pizza2 = new Pizza(8, 2, 0)
+    allPizza.push(pizza2);
+    let pizza1 = new Pizza(12, 1, 0)
+    allPizza.push(pizza1);
+    allrefresh();
+}
+
+function allrefresh(){
     refreshDisplayedPizza();
     deleteEvent();
     diameterEvent();
@@ -292,9 +343,4 @@ function popular1(){
     generalEvent();
 }
 
-refreshDisplayedPizza();
-deleteEvent();
-diameterEvent();
-quantityEvent();
-priceEvent();
-generalEvent();
+allrefresh();
